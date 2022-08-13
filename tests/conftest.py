af2acea96e92c92e749d310cded43a999a4ac625
scalser <scalserstv@yandex.ru>
2022-08-13 16:18:49 +0300

    added auto install chromedriver

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
 """fixtures for work with browser Chrome"""
 import pytest
+import chromedriver_autoinstaller
 from selenium.webdriver import Chrome
+from selenium.common.exceptions import WebDriverException
 
 
 @pytest.fixture(scope="function")
 def browser():
     """default browser -> GoogleChrome"""
-    browser = Chrome()
+    try:
+        browser = Chrome()
+    except WebDriverException:
+        chromedriver_autoinstaller.install()
+        browser = Chrome()
     browser.set_window_size(1920, 1080)
     yield browser
     browser.quit()
